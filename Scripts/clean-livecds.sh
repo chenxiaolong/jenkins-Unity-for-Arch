d05dccdf5aee688ce630b324bfa069a8b5e38bf3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-25 19:50:33 -0500

    Remove remaining live CD scripts

--- Scripts/clean-livecds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "x$(whoami)" != "xroot" ]; then
-  echo "This script must be run as root!"
-  exit 1
-fi
-
-find /srv/livecds/ -name 'Unity-for-Arch-*.iso' | sort | head -n -3 | xargs rm
